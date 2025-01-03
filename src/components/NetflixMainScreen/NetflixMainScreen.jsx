5f30c8855beba08a046851efed34ddd0ecb34619
Alyssa Resh <aresh@Zipcoders-MacBook-Pro.local>
2025-01-03 10:18:52 -0500

    updated with org repo

--- src/components/NetflixMainScreen/NetflixMainScreen.jsx
+++ src/components/NetflixMainScreen/NetflixMainScreen.jsx
@@ -7,16 +7,16 @@ import './embla.css';
 const OPTIONS: EmblaOptionsType = { dragFree: true, loop: true }
 const SLIDE_COUNT = 5
  const SLIDES = [
-    '/diehardcaro.png',
-    '/toystorycaro.png',
-    '/interstellarcaro.png',
+    'carousel-photos/diehardcaro.png',
+    'carousel-photos/toystorycaro.png',
+    'carousel-photos/interstellarcaro.png',
   ];
 
 const NetflixMainScreen = () => {
   return (
     <div style={componentNetflixScreenStyle}>
-      {/* Remove the text in next 2 lines */}
       <EmblaCarousel slides={SLIDES} options={OPTIONS} />
+      <h1>Continue Watching</h1>
     </div>
   );
 };
